4168da7f110892a32993b79058e1da08157aa749
Gerolmed <werwolf420@yahoo.de>
2023-07-01 18:35:04 +0200

    Reformatted category page

--- src/Category.tsx
+++ src/Category.tsx
@@ -1,6 +1,6 @@
 import {Question} from "./quiz/QuestionShape";
 import {Quiz} from "./quiz/Quiz";
-import {useState} from "react";
+import React, {useState} from "react";
 import "./Category.css"
 
 type CategoryProps = {
@@ -13,11 +13,13 @@ type CategoryProps = {
 
 export default function Category(props: CategoryProps) {
     const [quizOpen, setQuizOpen] = useState(false);
+
     const [stars, setStars] = useState(() => {
         const stars = parseInt(localStorage.getItem(props.id + "_stars") || "")
         return !isNaN(stars) ? stars : -1;
-
     });
+
+
     const resultHandler = (score: number, correct: number) => {
         const total = props.questionSet.length;
         const newStars = Math.floor(correct / total * 3);
@@ -33,10 +35,16 @@ export default function Category(props: CategoryProps) {
     if (quizOpen) {
         content = <Quiz questionSet={props.questionSet} onEnded={resultHandler}/>;
     } else {
+
+        const starCountLight = stars
+        const starCountDark = 3 - stars
+
         content = <>
-            <button onClick={() => setQuizOpen(true)}>open quiz</button>
-            <div>Number of stars: {stars}</div>
-            <button onClick={() => props.onExit()}>Back to Map</button>
+            <div className={"quiz-bar"}>
+                <button onClick={() => setQuizOpen(true)}>Try Quiz</button>
+                {stars >= 0 && <StarBar light={starCountLight} dark={starCountDark}/>}
+            </div>
+
             <div>{props.children}</div>
         </>
     }
@@ -44,10 +52,34 @@ export default function Category(props: CategoryProps) {
     return (
         <div className={"details-page"}>
             <div className={"body"}>
+                <button className={"world-map-button"} onClick={() => props.onExit()}>Back to World Map</button>
                 <h2>{props.name}</h2>
                 {content}
             </div>
         </div>
     )
 
+}
+
+type StarBarProps = {
+    light: number,
+    dark: number,
+}
+
+function StarBar(props: StarBarProps) {
+
+    const stars: JSX.Element[] = []
+
+    for (let i = 0; i < props.light; i++) {
+        stars.push(<span className={"star light"}>★</span>)
+    }
+    for (let i = 0; i < props.dark; i++) {
+        stars.push(<span className={"star dark"}>★</span>)
+    }
+
+
+    return <div className={"star-bar"}>
+        You achieved: {stars}
+        ({props.light}/3)
+    </div>
 }
